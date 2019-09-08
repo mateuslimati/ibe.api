6dfbe9ddaa76c9e46c37f12633563ea40bf341bc
Mateus Lima <mateuslima@trixlog.com>
2019-09-08 04:06:33 -0300

    Remove security package

--- src/main/java/com/gileadenm/ibe/api/security/SecurityConfig.java
+++
@@ -1,7 +0,0 @@
-package com.gileadenm.ibe.api.security;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-}
